a222c243b5f23ed46220f80b20a0149c29a807a5
Samantha Samuels <your_email@whatever.com>
2014-11-17 15:38:32 -0800

    added authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+   skip_before_filter :authorize
+   
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: "Thanks for signing up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
