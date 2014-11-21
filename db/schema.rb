e8d871a946b783ec2a70fe9ff483f98cabe15b4b
soutchay <chungvus@gmail.com>
2014-11-21 09:20:33 -0800

    rake db:migrate for schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119225722) do
+ActiveRecord::Schema.define(version: 20141121001003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 20141119225722) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "usergroups", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "usergroups", ["group_id"], name: "index_usergroups_on_group_id", using: :btree
+  add_index "usergroups", ["user_id"], name: "index_usergroups_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
