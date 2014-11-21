e4d576c00f39be65692159d6953ed1915a4bb3df
soutchay <chungvus@gmail.com>
2014-11-21 14:01:27 -0800

    added uniquness test for email under User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it { should validate_presence_of(:email)}
   it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:email)}
 end
