ce20b6ae9e40fef133159b27d9ec07e0ab1b76cf
soutchay <chungvus@gmail.com>
2014-11-21 18:08:41 -0800

    added has_many to lists, user has_many lists, add user_id to list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121200517) do
+ActiveRecord::Schema.define(version: 20141122020653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20141121200517) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "lists", ["movies_id"], name: "index_lists_on_movies_id", using: :btree
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
 
   create_table "movielists", force: true do |t|
     t.integer  "movie_id"
