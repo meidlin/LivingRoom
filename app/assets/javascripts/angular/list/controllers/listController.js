3276b6f3dd7c511a6d2fb209e23c1ffc8c4ee96e
soutchay <chungvus@gmail.com>
2014-11-26 16:41:20 -0800

    fix current user_id to list

--- app/assets/javascripts/angular/list/controllers/listController.js
+++ app/assets/javascripts/angular/list/controllers/listController.js
@@ -1,15 +1,22 @@
 angular.module('myApp')
 .controller('listController', function($scope, UserService, api){
 	$scope.currentUser = UserService.user;
+	console.log($scope.currentUser);
+
 	api.getLists()
 		.then(function(data){
 		$scope.lists = data.data;
 	});
 
+	api.getMovies()
+		.then(function(data){
+		$scope.movies = data.data;
+	});	
+		
 	$scope.addList = function(){
-		console.log()
-		api.createList($scope.listName, $scope.currentUser.id);
+		api.createList($scope.listName, $scope.currentUser);
 		// CLEAR the input after add planet
 		// $scope.lists.push(list);
 	};
+
 });
