ade0ed74e64ef535ef0f99609c3023f0497b58c3
Samantha Samuels <your_email@whatever.com>
2014-11-30 15:17:46 -0800

    created carousel

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,39 +1,51 @@
 $( document ).ready( function(){
   console.log('Samantha');
 
-});
-
 (function($) {
   var slider = $('div.slider').css('overflow', 'hidden'),
     imgs = slider.find('movie'),
-    imgWidth = imgs[0].width,
+    imgWidth = '368px', //hard coded width of scroll 
     imgsLen = imgs.length,
     current = 1,
     totalImgsWidth = imgsLen * imgWidth;
 
 $('#slider-nav').show().find('button').on('click', function(){
   var direction = $(this).data('dir');
+      loc = imgWidth;
+
 
   // update current value
 
   if ( direction === 'next') {
     current += 1; //2
   } else {
-      current -=1; //0
+      current -= 1; //0
   }
 
   if ( current === 0) {
       current = imgsLen;
-      direction === 'next'; 
+      loc = totalImgsWidth - imgWidth;
+      direction = 'next';
   } else if ( current -1 === imgsLen) {
       current = 1;
+      loc = 0;
   }
 
-  transition();
+  transition(slider, loc , direction);
 });
 
-function transition( container ) {
-  
+function transition( container, loc, direction ) {
+  var unit;
+
+  if (direction && loc !== 0) {
+    unit = (direction === 'next') ? '-=' : '+=';
+  }
+
+  container.animate({
+    'margin-left': unit ? ( unit + loc) : loc
+  });
 }
 
 })(jQuery);
+
+});
