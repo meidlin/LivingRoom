1f1d2c22045aed12b44fb8e7f9895b92a936875e
soutchay <chungvus@gmail.com>
2014-12-01 17:17:15 -0800

    delete copy files

--- app/models/user.rb.BASE.26775.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  include ActiveModel::SecurePassword
-
-  has_secure_password
-
-  validates_uniqueness_of :email
-  has_many :lists
-  has_many :movies, through: :lists
-end
